52fee4f8f8686c927c17323475d6db09df865846
Aldiyar <aissenbayev@one.kz>
2023-04-10 21:38:47 +0600

    Initial commit

---
+++ src/main/java/kz/iitu/pcsystem/PCSystemApplication.java
@@ -0,0 +1,13 @@
+package kz.iitu.pcsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PCSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PCSystemApplication.class, args);
+	}
+
+}
